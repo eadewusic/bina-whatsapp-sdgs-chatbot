7ac5ab66fafc685939fedc643ce4dab53336e066
Eunice Adewusi <e.adewusi@alustudent.com>
2024-07-30 02:52:20 +0200

    Bot now responds in caps

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,14 @@
 from flask import Flask
 from app.config import load_configurations, configure_logging
 from .views import webhook_blueprint
-import logging
+
 
 def create_app():
     app = Flask(__name__)
 
     # Load configurations and logging settings
     load_configurations(app)
-    logging.basicConfig(level=logging.INFO)
-    logging.info(f"Loaded VERIFY_TOKEN in __init__: {app.config.get('VERIFY_TOKEN')}")
+    configure_logging()
 
     # Import and register blueprints, if any
     app.register_blueprint(webhook_blueprint)
--- app/config.py
+++ app/config.py
@@ -1,13 +1,11 @@
-import os
 import sys
+import os
 from dotenv import load_dotenv
 import logging
 
+
 def load_configurations(app):
-    # Load the .env file
     load_dotenv()
-    
-    # Set configurations from environment variables
     app.config["ACCESS_TOKEN"] = os.getenv("ACCESS_TOKEN")
     app.config["YOUR_PHONE_NUMBER"] = os.getenv("YOUR_PHONE_NUMBER")
     app.config["APP_ID"] = os.getenv("APP_ID")
@@ -16,9 +14,7 @@ def load_configurations(app):
     app.config["VERSION"] = os.getenv("VERSION")
     app.config["PHONE_NUMBER_ID"] = os.getenv("PHONE_NUMBER_ID")
     app.config["VERIFY_TOKEN"] = "BINAC"
-    
-    # Debug statement to check VERIFY_TOKEN
-    logging.info(f"Loaded VERIFY_TOKEN: {app.config['VERIFY_TOKEN']}")
+
 
 def configure_logging():
     logging.basicConfig(
